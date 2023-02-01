fbf3207c858ec37af4944c201683d699a0d01faa
Spectrum5 <toskusergio@gmail.com>
2023-02-01 20:11:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+  components: {
+    AppTitle,
+  },
+
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+  <AppTitle/>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    La mia prima app con Vite!
+  </h1>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+
+h1{
+    color: red;
+}
+</style>
